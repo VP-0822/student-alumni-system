4973b2cdaf72fe35a18562bc456166f984ccfda4
Akansha <akanshanhce@gmail.com>
2019-05-04 01:52:38 +0200

    Added unfollow user query and creation of users event

--- src/webapp/controllers/userNodes.js
+++ src/webapp/controllers/userNodes.js
@@ -72,12 +72,42 @@ exports.getUsersEventsList = function(req, res, userName, handleSuccessResponse,
 
 exports.setUsersFollowers = function(req, res,sourceUser, destUser, handleSuccessResponse, handleErrorResponse){
     var nowDate = new Date();
-    var dateAsString = nowDate.getFullYear()+"-"+nowDate.getMonth()+"-"+nowDate.getDay();
-    var query = "match (a:user{name:'"+sourceUser+"'}) match(v:user{name:'"+destUser+"'}) create (a) -[f:FOLLOWING {since: \""+dateAsString+"\"}] -> (v) return a,v"
+    var dateAsString = nowDate.getFullYear()+"-"+nowDate.getMonth()+ 1 +"-"+nowDate.getDate();
+    var query = "match (a:user{name:'"+sourceUser+"'}) match(v:user{name:'"+destUser+"'}) create (a) -[f:FOLLOWING {since: date(\""+dateAsString+"\")}] -> (v) return a,v"
     session.run(query).then(function(result){
         var returnResults = [];
         result.records.forEach(element => {
             returnResults.push(element._fields[0].properties);
+            returnResults.push(element._fields[1].properties);
+        });
+        handleSuccessResponse(req, res, returnResults);
+    }).catch(function(err){
+        handleErrorResponse(req, res, err);
+    });
+}
+
+exports.unfollowList = function(req, res,sourceUser, destUser, handleSuccessResponse, handleErrorResponse){
+    var query = "match (a:user{name:'"+sourceUser+"'})-[f:FOLLOWING]->(v:user{name:'"+destUser+"'}) delete f"
+        session.run(query).then(function(result){
+        handleSuccessResponse(req, res,'User relationship deleted successfully');
+    }).catch(function(err){
+        handleErrorResponse(req, res, err);
+    });
+}
+
+exports.UsersParticipation = function(req, res,userName, organiser, eventId, handleSuccessResponse, handleErrorResponse){
+    var nowDate = new Date();
+    var dateAsString = nowDate.getFullYear()+"-"+nowDate.getMonth()+ 1 +"-"+nowDate.getDate();
+    var query = "match (a:user{name:'"+userName+"'}) match(e:event{event_id:'"+eventId+"'}) create (a) -[:PARTICIPATED_IN {date: date('"+dateAsString+"')";
+    if(organiser === 'true' || organiser === 'True') {
+        query += ", is_organiser: true";
+    }
+    query += "}] -> (e) return a,e";
+    session.run(query).then(function(result){
+        var returnResults = [];
+        result.records.forEach(element => {
+            returnResults.push(element._fields[0].properties);
+            returnResults.push(element._fields[1].properties);
         });
         handleSuccessResponse(req, res, returnResults);
     }).catch(function(err){
