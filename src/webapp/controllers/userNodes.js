f82ab79bbaba5d89d5f30bb8972d0f116e45c74d
Akansha <akanshanhce@gmail.com>
2019-05-03 21:36:05 +0200

    Adding apis. Adding followers list based on skill, create follow relationship, fetch all events of users

--- src/webapp/controllers/userNodes.js
+++ src/webapp/controllers/userNodes.js
@@ -1,6 +1,7 @@
+const neo4j = require('neo4j-driver').v1;
+
 exports.searchUsersByType = function(req, res, type, handleSuccessResponse, handleErrorResponse){
     var query = "match(n:user {type:'"+type+"'}) return n"
-    //"match(n:user{type:'student', name:'viraj'}) return n"
     session.run(query).then(function(result){
         var returnResults = [];
         result.records.forEach(element => {
@@ -14,7 +15,6 @@ exports.searchUsersByType = function(req, res, type, handleSuccessResponse, hand
 
 exports.getUsersFollowingList = function(req, res, userName, handleSuccessResponse, handleErrorResponse){
     var query = "match (n:user{name:'"+userName+"'})- [f:FOLLOWING]-> (r:user) return r"
-    //"match(n:user{type:'student', name:'viraj'}) return n"
     session.run(query).then(function(result){
         var returnResults = [];
         result.records.forEach(element => {
@@ -28,7 +28,52 @@ exports.getUsersFollowingList = function(req, res, userName, handleSuccessRespon
 
 exports.getUsersFollowersList = function(req, res, userName, handleSuccessResponse, handleErrorResponse){
     var query = "match (n:user{name:'"+userName+"'})<- [f:FOLLOWING]- (r:user) return r"
-    //"match(n:user{type:'student', name:'viraj'}) return n"
+    session.run(query).then(function(result){
+        var returnResults = [];
+        result.records.forEach(element => {
+            returnResults.push(element._fields[0].properties);
+        });
+        handleSuccessResponse(req, res, returnResults);
+    }).catch(function(err){
+        handleErrorResponse(req, res, err);
+    });
+}
+
+exports.getUsersFollowersSkillList = function(req, res, userName, handleSuccessResponse, handleErrorResponse){
+    var query = "match (n:user{name:'"+userName+"'})- [f:FOLLOWING]-> (k:user)-[:SKILLED_IN]->(p:skill) return p {.skill_name, users: collect(k {.name, .type})}"
+    session.run(query).then(function(result){
+        var returnResults = [];
+        result.records.forEach(element => {
+            returnResults.push(element._fields[0]);
+            //returnResults.push(element._fields[1].properties);
+        });
+        handleSuccessResponse(req, res, returnResults);
+    }).catch(function(err){
+        handleErrorResponse(req, res, err);
+    });
+}
+
+exports.getUsersEventsList = function(req, res, userName, handleSuccessResponse, handleErrorResponse){
+    var query = "match (n:user{name:'"+userName+"'}) - [p:PARTICIPATED_IN] ->(e:event) return n.name AS `User Name`, e.event_id As `Event Id`, e.event_name As `Event Name`, e.event_date.day+'/'+e.event_date.month+'/'+e.event_date.year As `Event Date`, CASE p.is_organiser WHEN true THEN 'Organiser' ELSE 'Participant' END As `Participation Type` ORDER BY e.event_date"
+    session.run(query).then(function(result){
+        var tableHeaderKeys;
+        result.records.forEach(element => {
+            tableHeaderKeys = element.keys;
+        });
+        var returnResults = [];
+        result.records.forEach(element => {
+            returnResults.push(element._fields);
+        });
+        handleSuccessResponse(req, res, {tableHeader: tableHeaderKeys, tableItems: returnResults});
+    }).catch(function(err){
+        handleErrorResponse(req, res, err);
+    });
+}
+
+exports.setUsersFollowers = function(req, res,sourceUser, destUser, handleSuccessResponse, handleErrorResponse){
+    var nowDate = new Date();
+    var dateAsString = nowDate.getFullYear()+"-"+nowDate.getMonth()+"-"+nowDate.getDay();
+    var query = "match (a:user{name:'"+sourceUser+"'}) match(v:user{name:'"+destUser+"'}) create (a) -[f:FOLLOWING {since: \""+dateAsString+"\"}] -> (v) return a,v"
     session.run(query).then(function(result){
         var returnResults = [];
         result.records.forEach(element => {
--- src/webapp/routes/userNodes.js
+++ src/webapp/routes/userNodes.js
@@ -18,6 +18,23 @@ router.get('/followers/:name', function(req, res){
     userNodesController.getUsersFollowersList(req, res, userName, handleSuccessResponse, handleErrorResponse); 
 });
 
+router.get('/followerskill/:name', function(req, res){
+    var userName = req.params.name;
+    userNodesController.getUsersFollowersSkillList(req, res, userName, handleSuccessResponse, handleErrorResponse); 
+});
+
+router.get('/events/:name', function(req, res){
+    var userName = req.params.name;
+    userNodesController.getUsersEventsList(req, res, userName, handleSuccessResponse, handleErrorResponse); 
+});
+
+
+router.get('/createfollower', function(req, res){
+    var sourceUser = req.query.sourceUser;
+    var destUser = req.query.destUser;
+    userNodesController.setUsersFollowers(req, res, sourceUser, destUser, handleSuccessResponse, handleErrorResponse); 
+});
+
 function handleSuccessResponse(req, res, responseData)
 {
     if(responseData)
@@ -30,7 +47,7 @@ function handleSuccessResponse(req, res, responseData)
 
 function handleErrorResponse(req, res, err)
 {
-    req.flash('error', err.message);
+    console.log(err);
     res.sendStatus(500);
 }
 
